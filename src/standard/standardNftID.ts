b061faa0681a8a5738c7fd0e008a9c0a86fd597e
bagaking <zfaustk@qq.com>
2019-09-04 05:50:16 +0800

    feat (nft_id): add method create

--- src/standard/standardNftID.ts
+++ src/standard/standardNftID.ts
@@ -1,5 +1,7 @@
 import {Standard} from "./standard";
-import {NftID} from "../const";
+import {GameID, NftID} from "../const";
+import {standard_game_id} from "./standardGameID";
+import {Error} from "tslint/lib/error";
 
 /**
  *  Standard NftID format
@@ -12,6 +14,19 @@ export class StandardNftID extends Standard<boolean> {
 
     test(nft_id: NftID): boolean {
         return nft_id.length === 18 && nft_id[0] === "1";
+    }
+
+    create(game_id: GameID, seq: number) {
+
+        if (!standard_game_id.test(game_id)) {
+            throw new Error(`create nft_id failed, invalid game_id ${game_id}`);
+        }
+
+        if (seq !== Math.round(seq) || seq < 0 || seq > 9999999999999) {
+            throw new Error(`create nft_id failed, invalid seq ${seq}`);
+        }
+
+        return `1${_.padStart(game_id.toFixed(0), 4, "0")}${_.padStart(seq.toFixed(0), 13, "0")}`;
     }
 
 
